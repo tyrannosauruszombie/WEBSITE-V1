d0758702f537654e3b928a65f0d331ed64c562da
Ben Dixon <bendixon@Mac.cust.communityfibre.co.uk>
2025-10-07 20:42:41 +0100

    Fix relative paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WEBSITE-V1/', // 👈 match your GitHub repo name exactly
+  base: './', // ✅ use relative paths for GitHub Pages subfolder sites
 })
