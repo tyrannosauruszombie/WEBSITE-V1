11baa3b2c3adcf3a77a776f06ddce033f43089e1
Ben Dixon <bendixon@Mac.cust.communityfibre.co.uk>
2025-10-07 20:37:38 +0100

    Add vite.config and rebuild for GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/WEBSITE-V1/', // 👈 match your GitHub repo name exactly
+})
